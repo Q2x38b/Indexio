1083df10e22cb9d04f6e4b1e4668dfbe01899985
Q2x38b <132852530+Q2x38b@users.noreply.github.com>
2024-11-09 00:36:01 -0600

    Delete Indexio directory

--- Indexio/scrapper.py
+++
@@ -1,6 +0,0 @@
-from autoscraper import AutoScraper 
-url = "https://stackoverflow.com/questions/2081586/web-scraping-with-python" 
-wanted_list = [""] 
-scraper = AutoScraper()
-result = scraper.build(url, wanted_list) 
-print (result)
